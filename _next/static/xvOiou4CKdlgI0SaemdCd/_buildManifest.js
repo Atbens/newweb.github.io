3d24c3a9a8259ed4367be71041dba2258ee2fc32
Atbens <100333831+Atbens@users.noreply.github.com>
2022-04-18 14:32:56 +0000

    Deploying to gh-pages from @ Atbens/newweb.github.io@ba8e2c383e07b6c9aa369c1901ac938d6b8b2db0 🚀

---
+++ _next/static/xvOiou4CKdlgI0SaemdCd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/061e6b60-28bd444bb64e34ae.js","static/chunks/230-e54299b2c942d310.js","static/chunks/pages/index-ba4ba52f9979790e.js"],"/_error":["static/chunks/pages/_error-a3f18418a2205cb8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
