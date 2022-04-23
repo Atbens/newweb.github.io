dae516256bdbde4f2c26133a8744440eaa12f538
Atbens <100333831+Atbens@users.noreply.github.com>
2022-04-23 17:27:29 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/index': { page: '/index' },
-      '/p/hello-nextjs': { page: '/post', query: { title: 'hello-nextjs' } },
-      '/p/learn-nextjs': { page: '/post', query: { title: 'learn-nextjs' } },
-      '/p/deploy-nextjs': { page: '/post', query: { title: 'deploy-nextjs' } },
-    }
-  },
-
-
+  reactStrictMode: true
 };
